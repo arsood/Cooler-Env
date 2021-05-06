2c94c3b328c9f0ef63d409cd995c6cf2abf88f94
Arun Sood <arsood@gmail.com>
2021-05-06 12:35:07 -0700

    Load environment function

--- loadEnv.js
+++ loadEnv.js
@@ -1,3 +1,20 @@
-const loadEnv = () => {
-  
+const loadEnv = (env) => {
+  const secretKeyData = fs
+  .readFileSync(path.join(__dirname, `./config/${env}.key`))
+  .toString();
+
+  const encryptedFileInstance = new Cryptify(path.join(__dirname, `../config/${env}.yml.enc`), secretKeyData, null, null, true, true);
+
+  encryptedFileInstance
+  .decrypt()
+  .then((files) => {
+    const parsedObj = JSON.parse(files[0]);
+
+    Object.keys(parsedObj).forEach((key) => {
+      process.env[key] = parsedObj[key];
+    });
+  })
+  .then(() => {
+    encryptedFileInstance.encrypt();
+  });
 }
