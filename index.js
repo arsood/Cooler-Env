b5a8854d1197a6a3abd11037ba6cf08e99c0135f
Arun Sood <arun.sood@coursehero.com>
2021-05-10 14:47:02 -0700

    Splash for index

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const chalk = require("chalk");
 const minimist = require("minimist");
+const clear = require("clear");
+const figlet = require("figlet");
 
 const init = require("./commands/init");
 const edit = require("./commands/edit");
@@ -9,6 +11,14 @@ const add = require("./commands/add");
 const deleteCmd = require("./commands/delete");
 
 const loadEnv = require("./loadEnv");
+
+clear();
+
+console.log(
+  chalk.green(
+    figlet.textSync("Cooler Env", { horizontalLayout: "full" })
+  )
+);
 
 const argv = minimist(process.argv.slice(2));
 
