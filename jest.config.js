f9103f26747f9a0bda62141afae8e73a78a8f297
Arun Sood <arsood@gmail.com>
2024-09-30 10:40:37 -0700

    Test runner

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    "^.+.ts?$": ["ts-jest", {}],
+    "^.+\\.(js)$": "babel-jest",
+  },
+  transformIgnorePatterns: ["node_modules/(?!(chalk)/)"],
+};
