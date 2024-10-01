4631b6ba93a7ac24957b88f949422a576d77bcf8
Arun Sood <arsood@gmail.com>
2024-10-01 11:23:27 -0700

    Transition add to async await

--- dist/commands/add.d.ts
+++ dist/commands/add.d.ts
@@ -1,3 +1,3 @@
-declare const add: (argv: any) => void;
+declare const add: (argv: any) => Promise<void>;
 export default add;
 //# sourceMappingURL=add.d.ts.map
--- src/commands/add.ts
+++ src/commands/add.ts
@@ -6,7 +6,7 @@ import chalk from "chalk";
 import figlet from "figlet";
 import Cryptify from "cryptify";
 
-const add = (argv: any) => {
+const add = async (argv: any) => {
   const CONFIG_DIR_PATH = path.join(process.cwd(), argv.p ? argv.p : "config");
   const ENCRYPTION_KEY_PATH = path.join(CONFIG_DIR_PATH, `${argv.e}.key`);
   const ENCRYPTED_FILE_PATH = path.join(CONFIG_DIR_PATH, `${argv.e}.yml.enc`);
@@ -39,8 +39,8 @@ const add = (argv: any) => {
     );
   }
 
-  inquirer
-    .prompt([
+  try {
+    const answers = await inquirer.prompt([
       {
         name: "keyName",
         type: "input",
@@ -65,55 +65,54 @@ const add = (argv: any) => {
           return "Please enter the value of the key you would like to add.";
         },
       },
-    ])
-    .then((answers: any) => {
-      fs.copyFileSync(ENCRYPTED_FILE_PATH, DECRYPTED_FILE_PATH);
+    ]);
 
-      const secretKeyData = fs.readFileSync(ENCRYPTION_KEY_PATH).toString();
+    fs.copyFileSync(ENCRYPTED_FILE_PATH, DECRYPTED_FILE_PATH);
 
-      const decryptedFileInstance = new Cryptify(
-        DECRYPTED_FILE_PATH,
-        secretKeyData,
-        undefined,
-        undefined,
-        true,
-        true
+    const secretKeyData = fs.readFileSync(ENCRYPTION_KEY_PATH).toString();
+
+    const decryptedFileInstance = new Cryptify(
+      DECRYPTED_FILE_PATH,
+      secretKeyData,
+      undefined,
+      undefined,
+      true,
+      true
+    );
+
+    const files = await decryptedFileInstance.decrypt();
+
+    fs.unlinkSync(DECRYPTED_FILE_PATH);
+
+    if (!files) return;
+
+    const parsedObj = JSON.parse(files[0]);
+
+    if (parsedObj[answers.keyName]) {
+      return console.log(
+        chalk.red("The key exists already. Try editing instead?")
       );
+    }
 
-      decryptedFileInstance
-        .decrypt()
-        .then((files) => {
-          fs.unlinkSync(DECRYPTED_FILE_PATH);
+    parsedObj[answers.keyName] = answers.keyValue;
 
-          if (!files) return;
+    fs.writeFileSync(ENCRYPTED_FILE_PATH, JSON.stringify(parsedObj));
 
-          const parsedObj = JSON.parse(files[0]);
+    const encryptedFileInstance = new Cryptify(
+      ENCRYPTED_FILE_PATH,
+      secretKeyData,
+      undefined,
+      undefined,
+      true,
+      true
+    );
 
-          if (parsedObj[answers.keyName]) {
-            return console.log(
-              chalk.red("The key exists already. Try editing instead?")
-            );
-          }
+    await encryptedFileInstance.encrypt();
 
-          parsedObj[answers.keyName] = answers.keyValue;
-
-          fs.writeFileSync(ENCRYPTED_FILE_PATH, JSON.stringify(parsedObj));
-
-          const encryptedFileInstance = new Cryptify(
-            ENCRYPTED_FILE_PATH,
-            secretKeyData,
-            undefined,
-            undefined,
-            true,
-            true
-          );
-
-          encryptedFileInstance.encrypt();
-        })
-        .then(() => {
-          console.log("Done! 🌟");
-        });
-    });
+    console.log("Done! 🌟");
+  } catch (e) {
+    console.log("🚫 Cooler-Env 🚫");
+  }
 };
 
 export default add;
